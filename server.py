c3b923846f1ef12d11892043b8350a4d08c06076
Egill Einarss <EgilEinarss@github.com>
2014-03-03 18:45:08 +0000

    server update5

--- server.py
+++ server.py
@@ -3,6 +3,17 @@
 # Echo server program
 import socket
 
+def html(h,l):
+    f = open('placeholder.home.html','w')
+    f.write('<html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" /><title>Gangleri</title></head><body>')
+    f.write("The tempature is " + str(h) +"°<br />The lights are ")
+    if l == 1:
+        f.write("on.")
+    else:
+        f.write("off.")
+    f.write("</body></html>")
+    f.close()
+
 HOST = ''                 # Symbolic name meaning the local host
 PORT = 8080              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,9 +21,12 @@ s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
 print 'Connected by', addr
-while 1:
-    data = conn.recv(1024)
-    if not data: break
-    print data
-    conn.send(data)
+#while 1:
+#    data = conn.recv(1024)
+#    if not data: break
+#    print data
+#    conn.send(data)
+heat = conn.recv(1024)
+light = conn.recv(1024)
+conn.send("25")
 conn.close()
